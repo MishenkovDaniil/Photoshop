1b9ece670e0a2809ba2525e5920f47668a97beda
MishenkovDaniil <mishenkovdaniil@mail.ru>
2023-10-10 02:15:49 +0300

    made window friend for header

--- src/widget/window/header/header.h
+++ src/widget/window/header/header.h
@@ -5,12 +5,11 @@
 #include <cassert>
 
 #include "../../widget.h"
-// #include "../../../../resources/font.ttf"
+#include "../window.h"
 
-
-static const int HEADER_HEIGHT = 30;
-static const int CHARACTER_SIZE = 20;
-static const Color TEXT_COLOR = Color (0, 0, 0);
+static const int HEADER_HEIGHT       = 30;
+static const int CHARACTER_SIZE      = 20;
+static const Color TEXT_COLOR        = Color (0, 0, 0);
 static const char *DEFAULT_FONT_FILE = "/home/daniil/programming/code/C++_projects/photoshop/resources/font.ttf";
 
 class Header : public Widget
@@ -30,10 +29,12 @@ public:
     void render (sf::RenderTarget &target) const override;
     bool on_mouse_pressed  (Mouse_key mouse_key, Vector &pos) override;
     bool on_mouse_released (Mouse_key mouse_key, Vector &pos) override;
-    bool on_mouse_moved    (Vector &new_pos) override;    /// x, y - absolute values 
+    bool on_mouse_moved    (Vector &new_pos) override;   
     bool on_keyboard_pressed  (Keyboard_key key) override;
     bool on_keyboard_released (Keyboard_key key) override;
     bool on_time (float delta_sec);
+
+    friend Window;
 };
 
 
