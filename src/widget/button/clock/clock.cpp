bdab6f83316b8da94db6b01eb6fc643a645f476b
MishenkovDaniil <mishenkovdaniil@mail.ru>
2023-10-11 03:05:31 +0300

    initial

---
+++ src/widget/button/clock/clock.cpp
@@ -0,0 +1,85 @@
+#include "clock.h"
+#include <stdlib.h>
+#include <string.h>
+#include <cstring>
+
+Clock::Clock (Vector lh_corner, int width, int height, Button_run_fn func, Window *controlled_window, float hours, float minutes, float seconds, void *arg, Color fill_color, int run_mask) :
+    Button (lh_corner, width, height, func, controlled_window, arg, fill_color ,run_mask),
+    hours_ (hours),
+    minutes_ (minutes), 
+    seconds_ (seconds) 
+{};
+
+Clock::~Clock () {};
+
+void Clock::render (sf::RenderTarget &target)
+{
+    Button::render (target);
+
+    sprintf (hrs, "%d", (int)hours_);
+    sprintf (min, "%d", (int)minutes_);
+    sprintf (sec, "%d", (int)seconds_);
+    sprintf (clck, "%s:%s:%s", hrs, min, sec);
+    
+    sf::Text text;
+    sf::Font font;
+    font.loadFromFile (font_path);
+    text.setString (clck);
+    text.setFont (font);
+    text.setFillColor (sf::Color::White);
+    text.setCharacterSize (CLOCK_CHARACTER_SIZE);
+
+    double text_width = text.findCharacterPos(8).x - text.findCharacterPos (0).x;
+    text.setPosition (lh_corner_.get_x () + (width_ - text_width) / 2, lh_corner_.get_y () + height_ / 2 - CLOCK_CHARACTER_SIZE / 2);
+    
+    // target.draw (button);
+    target.draw (text);
+}   
+
+bool Clock::on_mouse_pressed  (Mouse_key mouse_key, Vector &pos)
+{
+    return false;
+} 
+
+bool Clock::on_mouse_released (Mouse_key mouse_key, Vector &pos)
+{
+    return false;
+} 
+
+bool Clock::on_mouse_moved    (Vector &new_pos)
+{
+    return false;
+}   
+
+bool Clock::on_keyboard_pressed  (Keyboard_key key)
+{
+    return false;
+}   
+
+bool Clock::on_keyboard_released (Keyboard_key key)
+{
+    return false;
+}   
+
+bool Clock::on_time (float delta_sec)
+{
+    seconds_ += delta_sec;
+    if ((int)seconds_ > 59)
+    {
+        seconds_ = (int)seconds_ % 60;
+        minutes_++;
+
+        if ((int)minutes_ > 59)
+        {
+            minutes_ = (int)minutes_ % 60;
+            hours_++;
+
+            if ((int)hours_ > 23)
+            {
+               hours_ = (int)hours_ % 24;
+            }
+        }
+    }
+
+    return true;
+}   
---
+++ src/widget/button/clock/clock.h
@@ -0,0 +1,30 @@
+#include "../button.h"
+
+static const char *font_path = "resources/font.ttf";
+static const size_t CLOCK_CHARACTER_SIZE = 20;
+
+class Clock : public Button
+{
+    float hours_ = 0;
+    float minutes_ = 0;
+    float seconds_ = 0;
+
+    char hrs[3] = "";
+    char min[3] = "";
+    char sec[3] = "";
+
+    char clck[20] = "";
+
+public:
+    Clock (Vector lh_corner, int width, int height, Button_run_fn func, Window *controlled_window, float hours, float minutes, float seconds, void *arg = nullptr, Color fill_color = Color (0, 0, 0, 0), int run_mask = RELEASE_BUTTON);
+    ~Clock ();
+
+
+    void render (sf::RenderTarget &target)                    override;
+    bool on_mouse_pressed  (Mouse_key mouse_key, Vector &pos) override;
+    bool on_mouse_released (Mouse_key mouse_key, Vector &pos) override;
+    bool on_mouse_moved    (Vector &new_pos)                  override;
+    bool on_keyboard_pressed  (Keyboard_key key)              override;
+    bool on_keyboard_released (Keyboard_key key)              override;
+    bool on_time (float delta_sec)                            override;
+};
