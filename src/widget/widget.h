8fd97008aa17d6ada3c6bbb8b67a552b0b02968e
MishenkovDaniil <mishenkovdaniil@mail.ru>
2023-10-16 17:55:19 +0300

    add tranform class and transform stack in widget functions to treat every widget inn realtive coordinates

--- src/widget/widget.h
+++ src/widget/widget.h
@@ -4,9 +4,12 @@
 #include <SFML/Graphics.hpp>
 #include "../graphic_structures/vector/vector.h"
 #include "../graphic_structures/color/color.h"
+#include "../vector.h"
 
 static const Color BLACK = Color (0, 0, 0, 255);
 static const Color WHITE = Color (255, 255, 255, 255);
+static const double TRANSFORM_HEIGHT = 100;
+static const double TRANSFORM_WIDTH  = 100;
 
 enum Mouse_key
 {
@@ -123,14 +126,34 @@ enum Keyboard_key
     KeyCount
 };
 
+class Transform 
+{
+public:
+    Vector offset_;
+    Vector scale_;
+
+    Transform () {};
+    Transform (const Vector offset, const Vector scale = Vector (1, 1)) : offset_ (offset), scale_ (scale) {};//
+    // Transform (Vector lh_pos, Vector size)  : offset_ (lh_pos), scale_ (Vector (1, 1)) {}; //scale_ (TRANSFORM_WIDTH / size.get_x (), TRANSFORM_HEIGHT / size.get_y ()) {};
+    Transform (const Transform &transform) : offset_ (transform.offset_), scale_ (transform.scale_) {};
+
+    Transform unite(const Transform &prev) const 
+    {
+        Vector new_offset = Vector (offset_.get_x () * prev.scale_.get_x (), offset_.get_y () * prev.scale_.get_y ()) + prev.offset_;
+        return Transform (new_offset, Vector (scale_.get_x () * prev.scale_.get_x (), scale_.get_y ()  * prev.scale_.get_y ()));
+    };
+    Vector apply_transform (Vector &vector) {return Vector (vector.get_x () - offset_.get_x (), vector.get_y () - offset_.get_y ());};
+};
+
+
 class Widget 
 {
 public:
     virtual ~Widget () = default;
-    virtual void render (sf::RenderTarget &target) = 0;
-    virtual bool on_mouse_pressed  (Mouse_key mouse_key, Vector &pos) = 0;
-    virtual bool on_mouse_released (Mouse_key mouse_key, Vector &pos) = 0;
-    virtual bool on_mouse_moved    (Vector &new_pos) = 0;    /// x, y - absolute values 
+    virtual void render (sf::RenderTarget &target, M_vector<Transform> &transform_stack) = 0;
+    virtual bool on_mouse_pressed  (Mouse_key mouse_key, Vector &pos, M_vector<Transform> &transform_stack) = 0;
+    virtual bool on_mouse_released (Mouse_key mouse_key, Vector &pos, M_vector<Transform> &transform_stack) = 0;
+    virtual bool on_mouse_moved    (Vector &new_pos, M_vector<Transform> &transform_stack) = 0;    /// x, y - absolute values 
     virtual bool on_keyboard_pressed  (Keyboard_key key) = 0;
     virtual bool on_keyboard_released (Keyboard_key key) = 0;
     virtual bool on_time (float delta_sec) = 0;
