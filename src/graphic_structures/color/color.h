d074693269fb2e69ca2df9d5f7a8ff6ba27e31ba
MishenkovDaniil <mishenkovdaniil@mail.ru>
2023-10-19 02:56:21 +0300

    add some default color values

--- src/graphic_structures/color/color.h
+++ src/graphic_structures/color/color.h
@@ -21,4 +21,16 @@ public:
     operator sf::Color()const {return sf::Color (r_, g_, b_, a_);};
 };
 
+static const Color Transparent  = Color (0, 0, 0, 0);
+static const Color Red          = Color (255, 0, 0);
+static const Color Green        = Color (0, 255, 0);
+static const Color Blue         = Color (0, 0, 255);
+static const Color White        = Color (255, 255, 255);
+static const Color Black        = Color (0, 0, 0);
+static const Color Cyan         = Color (0, 255, 255);
+static const Color Purple       = Color (255, 0, 255);
+static const Color Yellow       = Color (255, 255, 0);
+static const Color Brown        = Color (165, 42, 42);
+static const Color Maroon       = Color (128, 0, 0);
+
 #endif /* COLOR_H */
