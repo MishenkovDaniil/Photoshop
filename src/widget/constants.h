efb05d74a6ff939276b0d7429b490d0b340e1782
MishenkovDaniil <mishenkovdaniil@mail.ru>
2023-10-24 00:23:43 +0300

    add file with constants

---
+++ src/widget/constants.h
@@ -0,0 +1,10 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#include "../graphic_structures/color/color.h"
+
+static const char *DEFAULT_FONT_FILE = "resources/font.ttf";
+static const Color TEXT_COLOR        = Black;
+
+
+#endif /* CONSTANTS_H */
