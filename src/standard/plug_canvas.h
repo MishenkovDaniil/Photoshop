1761bf230381239f9250a7263e19e08eba90ecf5
MishenkovDaniil <mishenkovdaniil@mail.ru>
2023-11-26 21:51:51 +0300

    add canvas functions

--- src/standard/plug_canvas.h
+++ src/standard/plug_canvas.h
@@ -16,29 +16,20 @@ namespace plug
     {
     public:
         virtual ~Canvas () = default;
-        virtual bool contains (int x, int y) = 0;
         
-        virtual Vec2d get_size () = 0;
-        virtual sf::IntRect &get_draw_rect () = 0;
-        virtual void set_draw_rect_offset (int left, int top) = 0;
-        virtual void set_draw_rect_size   (int width, int height) = 0;
-        virtual void setSelectionMask (SelectionMask &new_selection) = 0;
+        virtual Vec2d getSize () const = 0;
+        virtual void setSize(const Vec2d& size) = 0;
         virtual const SelectionMask &getSelectionMask () = 0;
+        virtual Color getPixel(size_t x, size_t y) const = 0;
+        virtual void setPixel(size_t x, size_t y, const Color& color) = 0;
+        virtual const Texture& getTexture(void) const = 0;
 
-        virtual sf::RenderTexture &getRenderTexture () = 0;
+        virtual unsigned int getNativeHandle(void) const = 0;             
+        virtual bool contains (int x, int y) = 0;                               //to rm
+
+        virtual void draw(const VertexArray& vertex_array) = 0;             
+        virtual void draw(const VertexArray& vertex_array, const Texture &texture) = 0;
     };
-    //Class Canvas
-//   virtual ~Canvas() = default;
-//   virtual void draw(const VertexArray& vertex_array) = 0;
-//   virtual void draw(const VertexArray& vertex_array,
-//                     const Texture&     texture) = 0;
-//   virtual unsigned int getNativeHandle(void) const = 0;
-//   virtual Vec2d getSize(void) const = 0;
-//   virtual void setSize(const Vec2d& size) = 0;
-//   virtual SelectionMask& getSelectionMask(void) = 0;
-//   virtual Color getPixel(size_t x, size_t y) const = 0;
-//   virtual void setPixel(size_t x, size_t y, const Color& color) = 0;
-//   virtual const Texture& getTexture(void) const = 0;
 }
 
 #endif /* PLUG_CANVAS_H */
