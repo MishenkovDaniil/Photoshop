498b801450ef3c50a4c2c2f76c054646d11a34a9
MishenkovDaniil <mishenkovdaniil@mail.ru>
2023-11-28 02:33:36 +0300

    add example of shared filter

---
+++ src/plugin/filter/my_filter.cpp
@@ -0,0 +1,27 @@
+#include "my_filter.h"
+
+extern "C" plug::Plugin *getMyFilter (size_t type)
+{
+    return ((plug::PluginGuid)type == plug::PluginGuid::Filter) ? new White_black_filter : nullptr;
+}
+
+void White_black_filter::apply_filter (plug::Canvas &canvas) const
+{
+    const plug::SelectionMask *filter_mask = &(canvas.getSelectionMask ());
+
+    unsigned int width  = canvas.getSize ().get_x ();
+    unsigned int height = canvas.getSize ().get_y ();
+
+    const plug::Texture &texture_img = canvas.getTexture ();
+
+    for (int idx = 0; idx < width * height; ++idx)
+    {
+        if (filter_mask->getPixel(idx % width, idx / width))
+        {
+            plug::Color prev_color = canvas.getPixel (idx % width, idx / width);
+            double new_color_val = (LUMA_R_PARAM * prev_color.r + LUMA_G_PARAM * prev_color.g + LUMA_B_PARAM * prev_color.b);
+            plug::Color new_color (new_color_val, new_color_val, new_color_val);
+            canvas.setPixel (idx % width, idx / width, new_color);
+        }
+    }
+}
---
+++ src/plugin/filter/my_filter.h
@@ -0,0 +1,39 @@
+#ifndef MY_FILTER_H
+#define MY_FILTER_H
+
+#include "../../standard/standard.h"
+#include "../../widget/window/canvas/canvas.h"
+#include "../../standard/plug_plugin.h"
+
+class MYFilter : public plug::Filter
+{
+    plug::Widget *filter_widget = nullptr;
+    size_t ref_num_ = 0;
+    PluginData plugin_data_;
+public:
+    MYFilter () = default;
+    ~MYFilter () = default;
+
+    virtual void apply_filter (plug::Canvas &canvas) const override = 0;
+    plug::Widget *getWidget () {return filter_widget;};
+
+    virtual plug::Plugin *tryGetInterface (size_t type)     override 
+        {return ((plug::PluginGuid)type == plug::PluginGuid::Filter) ? this : nullptr;};	
+    virtual void addReference (plug::Plugin *)              override 
+        {++ref_num_;};
+    virtual void release (plug::Plugin *)                   override 
+        {if (!(--ref_num_)) delete this;};
+    virtual const plug::PluginData *getPluginData () const  override 
+        {return &plugin_data_;};
+};
+
+class White_black_filter : public MYFilter
+{
+public:
+    White_black_filter () {};
+    ~White_black_filter () = default;
+
+    void apply_filter (plug::Canvas &canvas) const override;
+};
+
+#endif /* MY_FILTER_H */
