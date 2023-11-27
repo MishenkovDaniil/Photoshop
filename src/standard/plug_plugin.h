8f40095445658a1e7992d3166eea5c353ffa3738
MishenkovDaniil <mishenkovdaniil@mail.ru>
2023-11-28 00:48:16 +0300

    made methods public, destructors virtual

--- src/standard/plug_plugin.h
+++ src/standard/plug_plugin.h
@@ -7,18 +7,22 @@ namespace plug
 {
     class PluginData 
     {
+    public:
         virtual const char *getName () const = 0;
         virtual const char *getTexturePath () const = 0;
 
-        virtual ~PluginData () = 0;
+        virtual ~PluginData () = default;
     };
 
     class Plugin 
     {
+    public:
         virtual Plugin *tryGetInterface (size_t) = 0;	// аналог  QueryInterface
         virtual void addReference (Plugin *) = 0;
         virtual void release (Plugin *) = 0;
         virtual const PluginData *getPluginData () const = 0;
+        
+        virtual ~Plugin () = default;
     };
     
     enum class PluginGuid
