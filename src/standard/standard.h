a5b7477eb8185f60d76b5d1905076a9fdb3a9299
MishenkovDaniil <mishenkovdaniil@mail.ru>
2023-11-28 02:33:53 +0300

    standart header init

---
+++ src/standard/standard.h
@@ -0,0 +1,24 @@
+#ifndef MDN_STANDARD_H
+#define MDN_STANDARD_H
+
+#include "plug_canvas.h"
+#include "plug_colorpalette.h"
+#include "plug_event.h"
+#include "plug_filter.h"
+#include "plug_key_code.h"
+#include "plug_layout.h"
+#include "plug_mouse_button.h"
+#include "plug_plugin.h"
+#include "plug_rendertarget.h"
+#include "plug_texture.h"
+#include "plug_tool.h"
+#include "plug_transform_stack.h"
+#include "plug_transform.h"
+#include "plug_vertex.h"
+#include "plug_widget.h"
+#include "plug_selection_mask.h"
+#include "../graphic_structures/color/color.h"
+#include "../graphic_structures/vector/vector.h"
+
+
+#endif /* MDN_STANDARD_H */
