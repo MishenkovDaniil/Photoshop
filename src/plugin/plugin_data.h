0b2082389d3787079f5e54424763bfc0a98981b5
MishenkovDaniil <mishenkovdaniil@mail.ru>
2023-11-28 02:32:43 +0300

    init

---
+++ src/plugin/plugin_data.h
@@ -0,0 +1,22 @@
+#ifndef PLUGIN_DATA_H
+#define PLUGIN_DATA_H
+
+#include <iostream>
+#include "../standard/plug_plugin.h"
+
+class PluginData : public plug::PluginData
+{
+    char *name_ = nullptr;
+    char *path_ = nullptr;
+
+public:
+    PluginData () = default;
+    PluginData (const char *name, const char *texture_path);
+    
+    const char *getName () const override {return name_;};
+    const char *getTexturePath () const override {return path_;};
+
+    ~PluginData () = default;
+};
+
+#endif /* PLUGIN_DATA_H */
