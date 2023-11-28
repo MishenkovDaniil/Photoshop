fdedddd2f07cc63d806bbe708b971373de8c26f3
MishenkovDaniil <mishenkovdaniil@mail.ru>
2023-11-29 02:07:24 +0300

    add plugin data in shared filter

--- src/plugin/filter/my_filter.h
+++ src/plugin/filter/my_filter.h
@@ -6,12 +6,28 @@
 #include "../../standard/plug_plugin.h"
 #include "../plugin_data.h"
 
+
+class MPluginData : public plug::PluginData
+{
+    char *name_ = nullptr;
+    char *path_ = nullptr;
+
+public:
+    MPluginData () = default;
+    MPluginData (const char *name, const char *texture_path);
+    
+    const char *getName () const override {return name_;};
+    const char *getTexturePath () const override {return path_;};
+
+    ~MPluginData ();
+};
+
 class MYFilter : public plug::Filter
 {
 protected:
     plug::Widget *filter_widget = nullptr;
     size_t ref_num_ = 0;
-    PluginData plugin_data_;
+    MPluginData plugin_data_;
 
 public:
     MYFilter (const char *name, const char *texture_path = nullptr) : plugin_data_ (name, texture_path) {};
@@ -27,14 +43,14 @@ public:
         {++ref_num_;};
     virtual void release (plug::Plugin *)                   override 
         {if (!(--ref_num_)) delete this;};
-    virtual const plug::PluginData *getPluginData () const  override 
-        {return &plugin_data_;};
+    virtual const plug::PluginData *getPluginData () const {return &plugin_data_;};
 };
 
 class White_black_filter : public MYFilter
 {
 public:
     White_black_filter () : MYFilter ("White&black") {};
+    // White_black_filter () = default;
     ~White_black_filter () = default;
 
     void apply_filter (plug::Canvas &canvas) const override;
