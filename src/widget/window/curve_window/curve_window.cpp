a605631e62a68a64b875e01f4296aab1963bfa39
MishenkovDaniil <mishenkovdaniil@mail.ru>
2023-12-20 13:28:18 +0300

    fixed destructor mismatch bugs

--- src/widget/window/curve_window/curve_window.cpp
+++ src/widget/window/curve_window/curve_window.cpp
@@ -5,14 +5,14 @@ CurveWindow::CurveWindow (int width, int height, plug::Vec2d lh_pos, const char 
     Window (width, height, lh_pos, w_name, false, palette),
     apply_button  (plug::Vec2d (0, 200), 60, 30, "APPLY", 
                   plug::Color (200, 200, 200), plug::Color (200, 200, 200), apply_filter_button, edited_canvas, 
-                  new std::pair<void *, void *> (curve_filter, curve_tool), PRESS_BUTTON),
+                  new std::pair<Curve_filter *, CurveTool *> (curve_filter, curve_tool), PRESS_BUTTON),
     cancel_button (plug::Vec2d (0, 230), 60, 30, "CANCEL", 
                   plug::Color (200, 200, 200), plug::Color (200, 200, 200), cancel_filter_button, 
                   new std::pair<Window * ,plug::Canvas *> (this, edited_canvas), 
-                  new std::pair<void *, void *> (curve_filter, curve_tool), PRESS_BUTTON),
+                  new std::pair<Curve_filter *, CurveTool *> (curve_filter, curve_tool), PRESS_BUTTON),
     reset_button  (plug::Vec2d (0, 260), 60, 30, "RESET", 
                   plug::Color (200, 200, 200), plug::Color (200, 200, 200), reset_filter_button, edited_canvas, 
-                  new std::pair<void *, void *> (curve_filter, curve_tool), PRESS_BUTTON),
+                  new std::pair<Curve_filter *, CurveTool *> (curve_filter, curve_tool), PRESS_BUTTON),
     menu_ (plug::Vec2d (width - 60 - 2, HEADER_HEIGHT), 60, height - HEADER_HEIGHT)
 {
     menu_.setFillColor (plug::Transparent);
@@ -28,22 +28,22 @@ CurveWindow::~CurveWindow ()
 {
     if (apply_button.get_arg ())
     {
-        delete apply_button.get_arg ();
+        delete (std::pair<Curve_filter *, CurveTool *> *)apply_button.get_arg ();
         apply_button.set_arg (nullptr);
     }
     if (cancel_button.get_arg ())
     {
-        delete cancel_button.get_arg ();
+        delete  (std::pair<Curve_filter *, CurveTool *> *)cancel_button.get_arg ();
         cancel_button.set_arg (nullptr);
     }
     if (cancel_button.get_widget ())
     {
-        delete cancel_button.get_widget ();
+        delete (std::pair<Window * ,plug::Canvas *> *)cancel_button.get_widget ();
         cancel_button.set_widget (nullptr);
     }
     if (reset_button.get_arg ())
     {
-        delete reset_button.get_arg ();
+        delete  (std::pair<Curve_filter *, CurveTool *> *)reset_button.get_arg ();
         reset_button.set_arg (nullptr);
     }
 }
